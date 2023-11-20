be83bdd3fccb9ccdca107f892e8b53ee11468cdd
goodbyebada <goodbyebada@naver.com>
2023-11-20 15:05:16 +0900

    modify for hosting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  basePath: "/veenoverse_clone/",
+};
+
+module.exports = nextConfig;
